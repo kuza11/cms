8e38e9b2e4f1871f5fcfdb5fc089f65fc16dca86
zvolaja <zvolaja@student.uzlabina.cz>
2024-03-11 14:58:42 +0100

    edit page

--- src/pages/edit/index.tsx
+++ src/pages/edit/index.tsx
@@ -1,15 +1,30 @@
-import React from 'react';
-import dynamic from 'next/dynamic';
+import React from "react";
+import dynamic from "next/dynamic";
+import useSWR from "swr";
+import { PageData } from "../api/getPage";
+import styles from "../../styles/edit.module.css";
 
-const CustomEditor = dynamic( () => {
-  return import( '../../components/custom-editor' );
-}, { ssr: false } );
+const fetcher = (...args: Parameters<typeof fetch>) =>
+  fetch(...args).then((res) => res.json());
+
+const CustomEditor = dynamic(
+  () => {
+    return import("../../components/custom-editor");
+  },
+  { ssr: false }
+);
 
 function Index() {
+  const { data: page, error: pageError } = useSWR<PageData>(`/api/getPage?id=${pageNum}`, fetcher);
   return (
-    <CustomEditor
-      initialData='<h1></h1>'
-    />
+    <>
+      <div className={styles.header}>
+        
+      </div>
+      <div className={styles.cke}>
+        <CustomEditor initialData="<h1></h1>" />
+      </div>
+    </>
   );
 }
 
